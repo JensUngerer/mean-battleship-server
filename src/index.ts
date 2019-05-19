f6313f0052e4816e117e5b68afa6f78294c5669b
Jens Ungerer <github@jens-ungerer.de>
2019-05-19 20:22:04 +0200

    using same port in client and server via a constant in in the common - folder.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { App } from './app';
+import { ConfigSocketIo } from '../../common/src/config/configSocketIo';
+
 const app = new App();
 app.configureProvidedDist('./client/dist/client');
-app.listen(3030);
+app.listen(ConfigSocketIo.PORT);
