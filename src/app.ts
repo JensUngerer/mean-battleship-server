997ab017637b58d73c02be65a353b24326dd04ee
Jens Ungerer <github@jens-ungerer.de>
2019-05-19 19:21:05 +0200

    Adding communication-class and its usage in app.ts in order to forward an incomingMessage to the game-partner. (And initialization logic in order to setup a socketIo-connection (plus destroying a socketIo communication):

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,32 @@ import { Application, Response, Request } from 'express';
 import express from 'express';
 import { Server } from 'http';
 import http from 'http';
-import socketIo from 'socket.io';
+import socketIo, { Socket } from 'socket.io';
 import path from 'path';
+
+import { Communication } from './communication';
+
+import { ConfigSocketIo } from './../../common/src/config/configSocketIo';
+import { SocketIoSendTypes } from './../../common/src/communication/socketIoSendTypes';
+import { SocketIoReceiveTypes } from '../../common/src/communication/socketIoReceiveTypes';
+
+import { IMessage } from '../../common/src/communication/message/iMessage';
+import { ICoordinatesMessage } from './../../common/src/communication/message/iCoordinatesMessage';
+import { ITileStateMessage } from './../../common/src/communication/message/iTileStateMessage';
 
 export class App {
     private express: Application;
     private server: Server;
     private io: socketIo.Server;
 
+    private socketIdUserId: { [key: string]: string } = {};
+    private communication: Communication;
+
     constructor() {
         this.express = express();
         this.server = http.createServer(this.express);
         this.io = socketIo(this.server);
+        this.communication = new Communication(this.io);
     }
 
     public configureProvidedDist(relativePath: string) {
@@ -34,11 +48,6 @@ export class App {
             // console.log(pathStr);
             response.sendFile('index.html', { root: pathStr });
         });
-
-
-        // this.express.get('*', (request: Request, response: Response) => {
-        //     response.redirect('/');
-        // });
     }
 
     public listen(port: number) {
@@ -46,6 +55,46 @@ export class App {
         this.server.listen(port, () => {
             // DEBUGGING:
             console.log('listening on:' + port);
+        });
+
+        this.io.on(ConfigSocketIo.SOCKET_IO_CONNECT_ID, (socket: Socket) => {
+            // DEBUGGING:
+            console.log('client connected on port:' + port);
+
+            const socketId: string = socket.id;
+
+            socket.on(ConfigSocketIo.SOCKET_IO_DISCONNECT_ID, () => {
+                // DEBUGGING:
+                console.log('client disconnected on port:' + port);
+
+                this.communication.removeUser(this.socketIdUserId[socketId], socketId);
+                delete this.socketIdUserId[socketId];
+            });
+
+            socket.on(SocketIoSendTypes.StartGame, (userId: string) => {
+                this.socketIdUserId[userId] = userId;
+                this.communication.addUser(userId, socketId);
+            });
+
+            socket.on(SocketIoSendTypes.Coordinates, (incomingMessage: ICoordinatesMessage) => {
+                incomingMessage.type = SocketIoReceiveTypes.Coordinates;
+                this.communication.emit(incomingMessage);
+            });
+
+            socket.on(SocketIoSendTypes.TileState, (incomingMessage: ITileStateMessage) => {
+                incomingMessage.type = SocketIoReceiveTypes.TileState;
+                this.communication.emit(incomingMessage);
+            });
+
+            socket.on(SocketIoSendTypes.RemainingTileState, (incomingMessage: ITileStateMessage) => {
+                incomingMessage.type = SocketIoReceiveTypes.RemainingTileState;
+                this.communication.emit(incomingMessage);
+            });
+
+            socket.on(SocketIoSendTypes.GameWon, (incomingMessage: IMessage)=>{
+                incomingMessage.type = SocketIoReceiveTypes.GameWon;
+                this.communication.emit(incomingMessage);
+            });
         });
     }
 }
---
+++ src/communication.ts
@@ -0,0 +1,73 @@
+import { IMessage } from './../../common/src/communication/message/iMessage';
+import { SocketIoReceiveTypes } from './../../common/src/communication/socketIoReceiveTypes';
+import socketIo from 'socket.io';
+
+export class Communication {
+  private readonly NO_GAME_PARTNER_FOUND = '';
+  private readonly NO_MATCHING_GAME_PARTNER = '';
+  private userIdSocketId: any = {};
+  private usersMap: any = {};
+
+  constructor(private io: socketIo.Server) {}
+
+  public emit(msg: IMessage) {
+    msg.targetUserId = this.getTargetUser(msg.sourceUserId);
+    // http://stackoverflow.com/questions/24041220/sending-message-to-a-specific-id-in-socket-io-1-0
+    const targetSocketId: string = this.userIdSocketId[msg.targetUserId];
+    this.io.to(targetSocketId).emit(msg.type, msg);
+  }
+
+  public addUser(userId: string, socketId: string) {
+    this.userIdSocketId[userId] = socketId;
+    const foundGamePartner = this.searchMatchingGamePartner(userId);
+    if (foundGamePartner === this.NO_GAME_PARTNER_FOUND) {
+      this.usersMap[userId] = this.NO_MATCHING_GAME_PARTNER;
+    } else {
+      const beginningUserByGamble = this.gambleBeginningUser(
+        userId,
+        foundGamePartner
+      );
+      const msg: IMessage = {
+        type: SocketIoReceiveTypes.BeginningUser,
+        targetUserId: beginningUserByGamble,
+        sourceUserId: userId
+      };
+      this.emit(msg);
+    }
+  }
+
+  public removeUser(userId: string, socketId?: string) {
+    const gamePartnerUserId = this.usersMap[userId];
+    delete this.usersMap[userId];
+    if (this.usersMap.hasOwnProperty(gamePartnerUserId)) {
+      this.usersMap[gamePartnerUserId] = this.NO_MATCHING_GAME_PARTNER;
+    }
+    if (this.userIdSocketId[userId]) {
+      delete this.userIdSocketId[userId];
+    }
+  }
+
+  public getTargetUser(sourceUser: string) {
+    return this.usersMap[sourceUser];
+  }
+
+  private gambleBeginningUser(userId: string, foundGamePartner: string) {
+    const gamePartners = [userId, foundGamePartner];
+    const randomBoolean = Math.random() >= 0.5;
+    const randomInt = randomBoolean ? 1 : 0;
+    return gamePartners[randomInt];
+  }
+
+  private searchMatchingGamePartner(userId: string): string {
+    for (const user in this.usersMap) {
+      if (this.usersMap.hasOwnProperty(user)) {
+        if (this.usersMap[user] === this.NO_MATCHING_GAME_PARTNER) {
+          this.usersMap[user] = userId;
+          this.usersMap[userId] = user;
+          return user;
+        }
+      }
+    }
+    return this.NO_GAME_PARTNER_FOUND;
+  }
+}
